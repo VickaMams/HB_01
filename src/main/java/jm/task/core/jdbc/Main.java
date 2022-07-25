c99892b1db05ff06b3427fbe97f926f55318ae3b
Victoria2412 <nado122012@gmail.com>
2022-07-25 13:58:21 +0300

    Please verify

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,38 @@
 package jm.task.core.jdbc;
 
-public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main  {
+    public static void main(String[] args) throws Exception  {
+
+//        Util util = new Util();
+//        util.getConnection();
+        try {
+            UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
+
+            userDao.createUsersTable();
+
+            userDao.saveUser("Donald", "Trump", (byte) 76);
+
+            userDao.saveUser("Liz", "Truss", (byte) 45);
+
+            userDao.saveUser("Joseph Robinette", "Biden Jr.", (byte) 81);
+
+            userDao.saveUser("Boris", "Johnson)", (byte) 58);
+
+
+            userDao.removeUserById(1);
+            userDao.getAllUsers();
+            userDao.cleanUsersTable();
+            userDao.dropUsersTable();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,145 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+    private Util util = new Util();
+    private Connection connection = util.getConnection();
     public UserDaoJDBCImpl() {
 
     }
 
-    public void createUsersTable() {
+    public void createUsersTable() throws SQLException {
+        String sqlCommand = "CREATE TABLE  IF NOT EXISTS users (id BIGINT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(20), " +
+                "lastName VARCHAR(50), age TINYINT)";
+        connection.setAutoCommit(false);
+        try (Statement statement = util.getConnection().createStatement()) {
+            statement.executeUpdate(sqlCommand);
+            connection.commit();
+        } catch (SQLException e) {
+            if (connection != null ) connection.rollback();
+            e.printStackTrace();
+        } finally {
+            connection.setAutoCommit(true);
+            if (connection!= null) {
+                connection.close();
+            }
+        }
 
     }
 
-    public void dropUsersTable() {
+    public void dropUsersTable() throws SQLException {
+        String sqlCommand = "DROP TABLE IF EXISTS users";
+        Connection connection= util.getConnection();
+        connection.setAutoCommit(false);
+        try (Statement statement = util.getConnection().createStatement()){
+
+            statement.executeUpdate(sqlCommand);
+            connection.commit();
+        } catch (SQLException e) {
+            if (connection != null ) connection.rollback();
+            e.printStackTrace();
+        }finally {
+            connection.setAutoCommit(true);
+            if (connection!= null) {
+                connection.close();
+            }
+        }
 
     }
 
-    public void saveUser(String name, String lastName, byte age) {
+    public void saveUser(String name, String lastName, byte age) throws SQLException {
+        Connection connection = util.getConnection();
+        connection.setAutoCommit(false);
+        try (PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO users (name, " +
+                "lastName, age) VALUES (?, ?, ?)")) {
+
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
+            preparedStatement.executeUpdate();
+            System.out.println("User с именем "+name+" добавлен в базу данных");
+            connection.commit();
+
+
+        } catch (SQLException e) {
+            if (connection!= null) connection.rollback();
+           e.printStackTrace();
+        }   finally {
+            connection.setAutoCommit(true);
+            if (connection!= null) {
+                connection.close();
+            }
+        }
 
     }
 
-    public void removeUserById(long id) {
+    public void removeUserById(long id) throws SQLException {
+        Connection connection = util.getConnection();
+        connection.setAutoCommit(false);
+        try(PreparedStatement preparedStatement = util.getConnection().prepareStatement("DELETE FROM users " +
+                "WHERE ID = ?")){
+
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
+            connection.commit();
+        } catch (SQLException e) {
+            if (connection != null) connection.rollback();
+            e.printStackTrace();
+        }finally {
+            connection.setAutoCommit(true);
+            if (connection!= null) {
+                connection.close();
+            }
+        }
 
     }
 
-    public List<User> getAllUsers() {
-        return null;
+    public List<User> getAllUsers() throws SQLException {
+        List<User> users = new ArrayList<>();
+        try (PreparedStatement preparedStatement = util.getConnection().prepareStatement("SELECT  * FROM users")) {
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()){
+                String name = resultSet.getString("name");
+                String lastName = resultSet.getString("lastName");
+                byte age = resultSet.getByte("age");
+                users.add(new User ( name, lastName, age));
+            }
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+        }finally {
+
+            if (connection!= null) {
+                connection.close();
+            }
+        }
+        return users;
     }
 
-    public void cleanUsersTable() {
+    public void cleanUsersTable() throws SQLException {
+        String sqlCommand = "TRUNCATE TABLE users";
+        Connection connection = util.getConnection();
+        connection.setAutoCommit(false);
+        try (Statement statement = util.getConnection().createStatement()){
 
+            statement.executeUpdate(sqlCommand);
+            connection.commit();
+        } catch (SQLException e) {
+            if (connection != null ) connection.rollback();
+            e.printStackTrace();
+        }finally {
+            connection.setAutoCommit(true);
+            if (connection!= null) {
+                connection.close();
+            }
+        }
     }
 }
